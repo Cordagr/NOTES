50744ce52e6c582c3fef2208947c0cd3b8e8b40e
Cordagr <113870656+Cordagr@users.noreply.github.com>
2024-06-14 16:48:46 -0500

    Create NewMainNoteParsing.jsx

---
+++ NewMainNoteParsing.jsx
@@ -0,0 +1,36 @@
+var jsonData = JSON.parse(userMessage);
+for(var i = 0; i < jsonData.counters.length; i++)
+    {
+      var counter = jsonData[i];
+      //Accessing//  // data within counter.//data types as rows// 
+      
+    }
+
+
+
+
+
+
+
+
+    //Parsing body EX//
+    // "success": true
+    // "counters": [
+    // {
+    // "counter_owner_name : Jack"
+    // "counter_associated_users" : "Lacu"
+    // "note_id" : "124"
+    //  
+    //
+    //
+    // },
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // ]
+    // }
